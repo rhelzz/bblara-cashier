9c0d542a32d3b5b1b738e1a208e13fc8252c3524
r n helzz <150502279+rhelzz@users.noreply.github.com>
2025-02-02 19:27:33 +0700

    Title : Changed To Indonesian Language

--- resources/views/owner/product/index.blade.php
+++ resources/views/owner/product/index.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Tabel Product - Bblara</title>
+    <title>Tabel Produk - Bblara</title>
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Bootstrap Icons CDN -->
--- resources/views/owner/product/show.blade.php
+++ resources/views/owner/product/show.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Product - Bblara</title>
+    <title>Produk - Bblara</title>
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Bootstrap Icons CDN -->
